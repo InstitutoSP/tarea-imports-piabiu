20f18e9186cd15c0eab743790805f1a3a62cdeb8
piabiu <biurriarenasierrapia-sec@instituto-sanpablo.com>
2024-04-17 08:52:11 -0300

    tarealista

---
+++ funciones.py
@@ -0,0 +1,50 @@
+def suma(a,b):
+      sumar=(a+b)
+      return(sumar)
+def resta(a,b):
+    resta=(a-b)
+    return(resta)
+def multi(a,b):
+    mult=(a*b)
+    return(mult)
+def div(a,b):
+  div=(a/b)
+  return(div)
+
+def ordenar_mayor_a_menor(list1):
+
+    stop=True
+    listaord=[]
+    while stop:
+        val_max= list1[0]
+        for i in range(len(list1)):
+            x=list1[i]
+
+            if x > val_max:
+                val_max=x
+
+        listaord.append(val_max)
+        list1.remove(val_max)
+
+        if len(list1)==0:
+            stop=False
+    
+    return listaord
+
+def orden_menor_a_mayor(list2):
+    stop=True
+    listaord=[]
+    while stop:
+        val_min= list2[0]
+        for i in range(len(list2)):
+            x=list2[i]
+
+            if x < val_min:
+                val_min=x
+
+        listaord.append(val_min)
+        list2.remove(val_min)
+
+        if len(list2)==0:
+            stop=False
+    return listaord
---
+++ main.py
@@ -0,0 +1,35 @@
+import funciones as fn
+OP=True
+while OP:
+  print("OPCIONES:\n Sumar-1\n Restar-2\n Multiplicar-3\n Dividir-4\n Salir-5\n ")
+  Opc=int(input("Ingrese numero de opcion "))
+  if Opc==1:
+    a=int(input("Ingrese numero "))
+    b=int(input("Ingrese numero "))
+    print(fn.suma(a,b))
+  elif Opc==2:
+    a=int(input("Ingrese numero "))
+    b=int(input("Ingrese numero "))
+    print(fn.resta(a,b))
+  elif Opc==3:
+    a=int(input("Ingrese numero "))
+    b=int(input("Ingrese numero "))
+    print(fn.multi(a,b))
+  elif Opc==4:
+    a=int(input("Ingrese numero "))
+    b=int(input("Ingrese numero "))
+    print(fn.div(a,b))
+  elif Opc==5:
+    break
+  
+
+
+lista_desordenada1 = [1,5,9,8,2,65,45,78,95,32,111,55,8,-5]
+
+lista_ordenada1 = fn.ordenar_mayor_a_menor(lista_desordenada1)
+print("Lista ordenada por la función :",lista_ordenada1)
+
+lista_desordenada2= [51,18,89,65,4,2,3,5,96,85,74,14,25,36,32,65,98,87,45,12]
+
+lista_ordenada2 = fn.orden_menor_a_mayor(lista_desordenada2)
+print("Lista ordenada por la función :",lista_ordenada2)
